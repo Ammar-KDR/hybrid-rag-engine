5e98d06cbe9e9b531037e70269b2a1904647b64f
AmmarKhader <amm20230047@std.psut.edu.jo>
2026-09-12 17:53:10 +0300

    chore: ignore exploratory experiment artifacts

--- experiement/text_retrieval.py
+++
@@ -1,9 +0,0 @@
-from rag.embedding.service import EmbeddingService
-from rag.vector_store.qdrant import QdrantVectorStore
-from rag.retrieval.Dense_retrieval import DenseRetriever
-
-embd=EmbeddingService()
-qv=QdrantVectorStore()
-c=DenseRetriever(embd,qv,"test_insert")
-cc=c.retrieve("How can I recover a failed Kubernetes workload?")
-print(cc)
